0a15d1121880a0c33ad3728a944096b95e9eaf7b
Nour Hashem <nour.hashem@stud.htwk-leipzig.de>
2024-07-06 18:08:00 +0200

    kleine änderungen

--- src/main.ts
+++ src/main.ts
@@ -6,27 +6,56 @@ console.log('Script started successfully');
 
 let currentPopup: any = undefined;
 
-// Waiting for the API to be ready
-WA.onInit().then(() => {
-    console.log('Scripting API ready');
-    console.log('Player tags: ',WA.player.tags)
+function displayDoor(state: unknown) {
+    if (state === true) {
+        WA.room.showLayer('door/door_opened');
+        WA.room.hideLayer('door/door_closed');
+    } else {
+        WA.room.hideLayer('door/door_opened');
+        WA.room.showLayer('door/door_closed');
+    }
+}
 
+
+WA.onInit().then(async () => {
+    console.log('Scripting API ready');
+    console.log('Player tags: ', WA.player.tags);
+
+    
     WA.room.area.onEnter('clock').subscribe(() => {
         const today = new Date();
-        const time = today.getHours() + ":" + today.getMinutes();
-        currentPopup = WA.ui.openPopup("clockPopup", "It's " + time, []);
-    })
+        const time = `${today.getHours()}:${today.getMinutes()}`;
+        currentPopup = WA.ui.openPopup("clockPopup", `It's ${time}`, []);
+    });
 
-    WA.room.area.onLeave('clock').subscribe(closePopup)
+    WA.room.area.onLeave('clock').subscribe(closePopup);
 
-    // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
-    bootstrapExtra().then(() => {
+ 
+    WA.state.onVariableChange('doorState').subscribe((doorState) => {
+        displayDoor(doorState);
+    });
+
+    displayDoor(WA.state.doorState);
+
+    WA.room.onEnterLayer('doorsteps/inside_doorstep').subscribe(() => {
+        WA.state.doorState = true;
+    });
+
+    WA.room.onLeaveLayer('doorsteps/inside_doorstep').subscribe(() => {
+        WA.state.doorState = false;
+    });
+
+    
+    try {
+        await bootstrapExtra();
         console.log('Scripting API Extra ready');
-    }).catch(e => console.error(e));
+    } catch (e) {
+        console.error(e);
+    }
 
 }).catch(e => console.error(e));
 
-function closePopup(){
+function closePopup() {
     if (currentPopup !== undefined) {
         currentPopup.close();
         currentPopup = undefined;
